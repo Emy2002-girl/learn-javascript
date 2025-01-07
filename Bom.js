e024043c16db07e7bf93158a5e6ae2bf770129e7
Emy2002-girl <ranboukiman805@gmail.com>
2025-01-07 16:54:52 +0000

    adding codes

---
+++ Bom.js
@@ -0,0 +1,57 @@
+// window.alert("Have a Good day !");
+// alert("Have a good day ");
+
+// let confirmMesg = confirm("Are you sure ?");
+
+// console.log(confirmMesg);
+
+// if(confirmMesg === true){
+//     console.log("Action fais");
+// }
+// else{
+//     console.log("essayer");
+// }
+
+// let PromptMesg = prompt("choice a day :","enter par 3 caractère");
+
+//siteTimeout and clearTimeout :
+ 
+// let counter = setTimeout(sayMsg,2000,"Imane",22);
+
+// function sayMsg(user,age){
+//     console.log(`Hello From tangier I'm ${user} ${age} years old`);
+// }
+
+// let mybtn = document.querySelector("button");
+
+// mybtn.onclick = function(){
+//     clearTimeout(counter);
+// }
+
+// setInterval(function(){
+//    console.log("Have a good day");
+// },1000)
+
+// function sayGood(){
+//     console.log("Have a good day Imane ");
+// }
+
+// setInterval(sayGood,1000);
+
+// let mydiv=document.querySelector("div");
+
+// function na9es(){
+//     mydiv.innerHTML -= 1;
+//     if(mydiv.innerHTML === "0"){
+//         clearTimeout(counter);
+//     }
+// };
+//  let counter =setInterval(na9es,1000);
+
+//Open and close window :
+  
+// setTimeout(function(){
+//    window.open("https://www.google.com","_blank","width:200,height: 40");
+// },2000);
+
+
